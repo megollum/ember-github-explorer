70e7c36bdb708a986dc7c85cec3df6e22ff09c97
Tarik Eminagic <megollum@gmail.com>
2017-05-17 18:39:21 +0200

    fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    loading: false,
     gitModel: null,
-
-    showSpinner: Ember.computed('loading', function () {
-        return this.get('loading');
-    }),
 
     gitResult: Ember.computed('gitModel', function(){
         return this.get('gitModel');
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,18 @@
 import Ember from 'ember';
-import _ from 'underscore';
 
 export default Ember.Route.extend({
+    spinner: Ember.inject.service('spinner'),
     beforeModel() {
         if (this.controller) {
-            this.set('controller.loading', false);
             this.set('controller.gitModel', null);
         }
     },
     actions: {
         performSearch(value) {
+            this.set('controller.gitModel', null);
             if (value) {
-                this.set('controller.loading', true);
+                this.get('spinner').show('123');
                 Ember.run.debounce(this, this._doSearch, { searchTerm: value }, 1000);
-            } else {
-                this.set('controller.loading', false);
-                this.set('controller.gitModel', null);
             }
         }
     },
@@ -23,8 +20,8 @@ export default Ember.Route.extend({
         let self = this;
         if (args.searchTerm) {
             Ember.$.getJSON("https://api.github.com/search/users?q=" + args.searchTerm).then(function (data) {
-                self.set('controller.loading', false);
                 self.set('controller.gitModel', data);
+                self.get('spinner').hide('123');                
             });
         }
     }
